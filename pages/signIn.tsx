f76fc2314692c2cbe7696c40c1844e34aa5aeb6c
Matthew Harvey <matthewtlharvey@gmail.com>
2022-10-31 22:43:39 +0000

    login submission loading states

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -1,5 +1,6 @@
 import {Button, Container, TextField, Card} from '@mui/material/';
-import { useState, useRef } from 'react';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Grid from '@mui/material/Grid';
 import BugReportOutlinedIcon from '@mui/icons-material/BugReportOutlined';
@@ -10,6 +11,9 @@ import axios from 'axios';
 
 export default function signIn() {
   const [isShown, setIsShown] = useState(false);
+  const [demoisLoading, setDemoLoading] = useState(false);
+  const [loginLoading, setLoginLoading] = useState(false);
+  const [registerLoading, setRegisterLoading] = useState(false);
 
   var [registerUsername, setRegisterUsername] = useState("");
   const handleregisterUsername = e => {
@@ -54,6 +58,7 @@ export default function signIn() {
       router.push({pathname: '/home', query: { username: loginUsername, pass: loginPassword}}, '/home', { shallow: true });
     } else {
       document.getElementById("err").innerText = "User not found.";
+      setLoginLoading(false);
     }
   }
 
@@ -72,6 +77,7 @@ export default function signIn() {
       router.push({pathname: '/home', query: { username: registerUsername, pass: registerPassword}}, '/home', { shallow: true });
     } else {
       document.getElementById("err").innerText = getValid.data.error;
+      setRegisterLoading(false);
     }
   }
 
@@ -86,50 +92,46 @@ export default function signIn() {
     <>
       <Container id="main" className="App">
         {!isShown && (
-          <form onSubmit={handleLogin}>
-            <Grid container spacing={0} style={{justifyContent: "center", textAlign: "center"}}>
-              <Grid item={true} xs={10}>
-                <Card variant="outlined" style={{padding: "3em", paddingTop: "0em"}}>
-                  <h1 style={{marginTop: "1em"}}>IssueTrack</h1>
-                  <BugReportOutlinedIcon fontSize='large'/>
-                  <h3 style={{marginTop: "1em"}}><u>Login</u></h3>
-                  <TextField style={{margin: "1em"}} required id="username" label="Username" value={loginUsername} onChange={handleloginUsername} />
-                  <br />
-                  <TextField type="password" style={{margin: "1em"}} required id="password" label="Password" value={loginPassword} onChange={handleloginPassword} />
-                  <br />
-                  <Button style={{margin: "1em"}} type="submit" variant="contained" color="primary" size="large">submit</Button>
-                  <br />
-                  <p id="err"></p>
-                  <Button style={{margin: "1em", backgroundColor: "grey"}} variant="contained" size="large" onClick={changestate}>Register</Button>
-                  <Button style={{margin: "1em", backgroundColor: "grey"}} variant="contained" size="large" onClick={Demo}>Demo Login</Button>
-                </Card>
-              </Grid>
+          <Grid container spacing={0} style={{justifyContent: "center", textAlign: "center"}}>
+            <Grid item={true} xs={10}>
+              <Card variant="outlined" style={{padding: "3em", paddingTop: "0em"}}>
+                <h1 style={{marginTop: "1em"}}>IssueTrack</h1>
+                <BugReportOutlinedIcon fontSize='large'/>
+                <h3 style={{marginTop: "1em"}}><u>Login</u></h3>
+                <TextField style={{margin: "1em"}} required id="username" label="Username" value={loginUsername} onChange={handleloginUsername} />
+                <br />
+                <TextField type="password" style={{margin: "1em"}} required id="password" label="Password" value={loginPassword} onChange={handleloginPassword} />
+                <br />
+                <LoadingButton loading={loginLoading} style={{margin: "1em"}} type="submit" variant="contained" color="primary" size="large" onClick={function(e) { setLoginLoading(true); handleLogin(e)}}>submit</LoadingButton>
+                <br />
+                <p id="err"></p>
+                <Button style={{margin: "1em", backgroundColor: "grey"}} variant="contained" size="large" onClick={changestate}>Register</Button>
+                <LoadingButton loading={demoisLoading} style={{margin: "1em", backgroundColor: "grey"}} variant="contained" size="large" id="demologin" onClick={function(e) { setDemoLoading(true); Demo(e)}}>Demo Login</LoadingButton>
+              </Card>
             </Grid>
-          </form>
+          </Grid>
         )}
         {isShown && (
-          <form onSubmit={handleRegister}>
-            <Grid container spacing={0} style={{justifyContent: "center", textAlign: "center"}}>
-              <Grid item={true} xs={10}>
-                <Card variant="outlined" style={{padding: "3em", paddingTop: "0em"}}>
-                  <h1 style={{marginTop: "1em"}}>IssueTrack</h1>
-                  <BugReportOutlinedIcon fontSize='large'/>
-                  <h3 style={{marginTop: "1em"}}><u>Register</u></h3>
-                  <TextField style={{margin: "1em"}} required id="register_username" label="Username" value={registerUsername} onChange={handleregisterUsername} />
-                  <br />
-                  <TextField type="email" style={{margin: "1em"}} required id="register_email" label="Email" value={registerEmail} onChange={handleregisterEmail} />
-                  <br />
-                  <TextField type="password" style={{margin: "1em"}} required id="register_password" label="Password" value={registerPassword} onChange={handleregisterPassword} />
-                  <br />
-                  <TextField type="password" style={{margin: "1em"}} required id="conf_password" label="Confirm Password" value={registerConf} onChange={handleregisterConf} />
-                  <br />
-                  <Button style={{margin: "1em", backgroundColor: "grey"}} variant="contained" size="large" onClick={changestate}>Login</Button>
-                  <Button style={{margin: "1em"}} type="submit" variant="contained" color="primary" size="large">submit</Button>
-                  <p id="err"></p>
-                </Card>
-              </Grid>
+          <Grid container spacing={0} style={{justifyContent: "center", textAlign: "center"}}>
+            <Grid item={true} xs={10}>
+              <Card variant="outlined" style={{padding: "3em", paddingTop: "0em"}}>
+                <h1 style={{marginTop: "1em"}}>IssueTrack</h1>
+                <BugReportOutlinedIcon fontSize='large'/>
+                <h3 style={{marginTop: "1em"}}><u>Register</u></h3>
+                <TextField style={{margin: "1em"}} required id="register_username" label="Username" value={registerUsername} onChange={handleregisterUsername} />
+                <br />
+                <TextField type="email" style={{margin: "1em"}} required id="register_email" label="Email" value={registerEmail} onChange={handleregisterEmail} />
+                <br />
+                <TextField type="password" style={{margin: "1em"}} required id="register_password" label="Password" value={registerPassword} onChange={handleregisterPassword} />
+                <br />
+                <TextField type="password" style={{margin: "1em"}} required id="conf_password" label="Confirm Password" value={registerConf} onChange={handleregisterConf} />
+                <br />
+                <Button style={{margin: "1em", backgroundColor: "grey"}} variant="contained" size="large" onClick={changestate}>Login</Button>
+                <LoadingButton loading={registerLoading} style={{margin: "1em"}} type="submit" variant="contained" color="primary" size="large" id="submitlogin" onClick={function(e) { setRegisterLoading(true); handleRegister(e)}}>submit</LoadingButton>
+                <p id="err"></p>
+              </Card>
             </Grid>
-          </form>
+          </Grid>
         )}
       </Container>
     </>
