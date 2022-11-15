428e0d8555cd89d2dbfb48b1821f660380121cda
Matthew Harvey <matthewtlharvey@gmail.com>
2022-11-15 21:08:28 +0000

    question cards - home

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,13 +10,13 @@ export default function page404() {
   return (
     <>
         <Grid container spacing={0} style={{justifyContent: "center", textAlign: "center", border: "1px"}}>
-            <Grid item={true} xs={6} sm={6} md={6} lg={6}>
+            <Grid item={true} xs={6} sm={6} md={5} lg={4}>
                 <Lottie animationData={Animation404} />
                 <p style={{padding: "1em"}}>Page not found.</p>
             </Grid>
         </Grid>
         <Grid container spacing={0} style={{justifyContent: "center", alignItems: "center", textAlign: "center"}}>
-            <Grid item={true} xs={6} sm={6} md={6} lg={6}>
+            <Grid item={true} xs={12} sm={12} md={12} lg={12}>
                 <IconButton href="/home">
                     <HomeIcon fontSize="large" style={{fill: "", color: "blue"}} />
                 </IconButton>
--- pages/home.tsx
+++ pages/home.tsx
@@ -2,11 +2,12 @@ import Mynav from './comps/Mynav';
 import { getCookie } from 'cookies-next';
 import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
-import { Avatar, Box, Button, Card, CircularProgress, Grid, Link } from '@mui/material';
+import { Avatar, Box, Breadcrumbs, Button, Card, CircularProgress, Grid, Link } from '@mui/material';
 import Footer from './comps/Footer';
 import Lottie from "lottie-react";
 import WelcomeAnimation from "../public/Welcome.json";
 import StatsAnimation from "../public/StatsAnimation.json";
+import BuilderAnimation from "../public/BuilderAnimation.json";
 
 const gradients = [
     ["linear-gradient(to bottom, #6C962B, #C66544)"],
@@ -74,13 +75,17 @@ export default function home() {
                 <div style={{position: "sticky", top: 0, zIndex: 100}}>
                     <Mynav params={{username: userid}}/>
                 </div>
-                <Grid container spacing={0} style={{alignItems: "center", padding: "1em"}}>
+                <Breadcrumbs aria-label="breadcrumb" style={{paddingTop: "1em", paddingLeft: "2em"}}>
+                    <Link underline="hover" color="inherit" href="/">IssueTrack</Link>
+                    <Link underline="hover" color="text.primary" href="/home" aria-current="page">Home</Link>
+                </Breadcrumbs>
+                <Grid container spacing={0} style={{alignItems: "center", padding: "1em", paddingTop: 0}}>
                     <Grid item={true} xs={12} sm={12} md={6} lg={6}>
                         <Lottie animationData={WelcomeAnimation} />
                     </Grid>
-                    <Grid item={true} xs={12} sm={12} md={6} lg={6}>
+                    <Grid item={true} xs={12} sm={12} md={6} lg={6} style={{padding: "2em"}}>
                         <Card variant='outlined' style={{padding: "3em"}}>
-                            <p style={{justifyContent: "center", textAlign: "center", padding: "1em"}}>Logged in as {userid}</p>
+                            <Lottie animationData={BuilderAnimation} style={{width: 350, height: 350, alignSelf: "center", margin: "0 auto"}} />
                             <p style={{padding: "1em"}}>IssueTrack is a tracking system for teams and individuals to create, resolve and close issues related to a project of their choice!
                                 Modelled using Microsoft Planner and Github issues; the aim is to create a comprehensive platform that covers gaps in the existing systems.
                                 Click below to start a new issue/team and begin tracking your goals...
@@ -90,47 +95,68 @@ export default function home() {
                             <Button style={{padding: "1em", margin: "1em"}} href="/create-team" variant='contained'>Create new team</Button>
                         </Card>
                     </Grid>
-                    <Grid item={true} xs={12} sm={12} md={12} lg={12}>
-                        <Grid container spacing={0} style={{alignItems: "center", padding: "1em"}}>
-                            <Grid item={true} xs={6} sm={6} md={4} lg={3} style={{padding: "1em", justifyContent: "center", textAlign: "center"}}>
-                                <h2 style={{padding: "1em"}}>Your Teams:</h2>
+                            <Grid item={true} xs={12} sm={12} md={12} lg={12}>
+                                <Grid container spacing={0} style={{alignItems: "center", padding: "1em"}}>
+                                    <Grid item={true} xs={6} sm={6} md={4} lg={3} style={{padding: "1em", justifyContent: "center", textAlign: "center"}}>
+                                        <h2 style={{padding: "1em"}}>Your Teams:</h2>
+                                    </Grid>
+                                    {userTeams.map((team, _key) => {
+                                        var getgradient = gradients[getRandom()][0];
+                                        var teamurl = "/teams/" + team;
+                                        return (
+                                            <>
+                                                <Grid item={true} key={_key} xs={6} sm={6} md={4} lg={3} style={{padding: "1em", justifyContent: "center", textAlign: "center"}}>
+                                                    <Card variant="outlined" style={{background: getgradient, padding: "1em"}}>
+                                                            <h3 style={{color: "white", padding: "1em"}}>{team}</h3>
+                                                            <Button style={{margin: "1em"}} href={teamurl} variant='contained'>View Team</Button>
+                                                    </Card>
+                                                </Grid>
+                                            </>
+                                        )
+                                    })}
+                                </Grid>
                             </Grid>
-                            {userTeams.map((team, _key) => {
-                                var getgradient = gradients[getRandom()][0];
-                                var teamurl = "/teams/" + team;
-                                return (
-                                    <>
-                                        <Grid item={true} key={_key} xs={6} sm={6} md={4} lg={3} style={{padding: "1em", justifyContent: "center", textAlign: "center"}}>
-                                            <Card variant="outlined" style={{background: getgradient, padding: "1em"}}>
-                                                    <h3 style={{color: "white"}}>{team}</h3>
-                                                    <br />
-                                                    <Button style={{}} href={teamurl} variant='contained'>View Team</Button>
-                                            </Card>
-                                        </Grid>
-                                    </>
-                                )
-                            })}
+                    <Grid item={true} xs={12} sm={12} md={12} lg={12}>
+                        <Grid container style={{alignItems: "center", padding: "1em", textAlign: "center"}}>
+                            <Grid item={true} xs={12} sm={12} md={12} lg={12}>
+                                <Lottie animationData={StatsAnimation} style={{width: 350, height: 350, alignSelf: "center", margin: "0 auto"}} />
+                                <h1>{userid}</h1>
+                                <h4>Status: {userStatus}</h4>
+                                <h5>Joined: {createdUser}</h5>
+                                <Button style={{margin: "1em", marginLeft: "0em"}} href={"/user/" + userid} variant='contained'>Profile</Button>
+                            </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item={true} xs={12} sm={12} md={12} lg={12}>
-                        <Card variant='outlined' style={{padding: "2em", paddingTop: "3em"}}>
-                            <Grid container spacing={0} style={{alignItems: "center", padding: "1em"}}>
-                                <Grid item={true} xs={12} sm={12} md={6} lg={6}>
-                                    <Avatar src="/vercel.svg" sx={{ width: 250, height: 250 }} style={{alignItems: 'center'}}/>
-                                    <h1>{userid}</h1>
-                                    <h4>Status: {userStatus}</h4>
-                                    <h5>Joined: {createdUser}</h5>
-                                    <Button style={{margin: "1em", marginLeft: "0em"}} href={"/user/" + userid} variant='contained'>Profile</Button>
-                                </Grid>
-                                <Grid item={true} xs={12} sm={12} md={6} lg={6}>
-                                    <Lottie animationData={StatsAnimation} />
-                                </Grid>
-                            </Grid>
+                    <Grid item={true} xs={12} sm={6} md={4} lg={4} style={{textAlign: "center", padding: "2em"}}>
+                        <Card variant="outlined" style={{background: "linear-gradient(-150deg,#424141 15%,#4e5056 70%,#505b6b 94%)", padding: "1em", borderRadius: "15px"}}>
+                            <h3 style={{color: "white"}}>Want to contribute?</h3>
+                            <br />
+                            <Button style={{textAlign: "center", color: "white", border: "1px solid #bbb", padding: "0.5em", alignSelf: "flex-end", textDecorationColor: "white"}} 
+                                href="https://github.com/Matthew-Harvey/IssueTrack/pulls" target="_blank">View Pull Requests
+                            </Button>
                         </Card>
                     </Grid>
-                    <Grid item={true} xs={12} sm={12} md={4} lg={4}>
+                    <Grid item={true} xs={12} sm={6} md={4} lg={4} style={{textAlign: "center", padding: "2em"}}>
+                        <Card variant="outlined" style={{background: "linear-gradient(to right top, #bcc4d1, #9eb4d8, #7da4df, #5894e5, #1284eb)", padding: "1em", borderRadius: "15px"}}>
+                            <h3 style={{color: "white"}}>Any Issues?</h3>
+                            <br />
+                            <Button style={{textAlign: "center", color: "white", border: "1px solid #bbb", padding: "0.5em", alignSelf: "flex-end", textDecorationColor: "white"}}
+                                href="https://mharvey.netlify.app/#contact" target="_blank">Contact Me
+                            </Button>
+                        </Card>
+                    </Grid>
+                    <Grid item={true} xs={12} sm={6} md={4} lg={4} style={{textAlign: "center", padding: "2em"}}>
+                        <Card variant="outlined" style={{background: "linear-gradient(to right top, #c053d1, #b849d4, #af40d7, #a436da, #982dde)", padding: "1em", borderRadius: "15px"}}>
+                            <h3 style={{color: "white"}}>Read more?</h3>
+                            <br />
+                            <Button style={{textAlign: "center", color: "white", border: "1px solid #bbb", padding: "0.5em", alignSelf: "flex-end", textDecorationColor: "white"}}
+                                href="https://github.com/Matthew-Harvey/IssueTrack/" target="_blank">IssueTrack docs
+                            </Button>
+                        </Card>
+                    </Grid>
+                    <Grid item={true} xs={12} sm={12} md={12} lg={12} style={{textAlign: "center", padding: "2em"}}>
                         <h2>Any Issues?</h2>
-                        <p>Please contact me <Link href="https://mharvey.netlify.app/#contact">here.</Link></p>
+                        <p>Please contact me <Link href="https://mharvey.netlify.app/#contact" target="_blank">here.</Link></p>
                     </Grid>
                 </Grid>
                 <Footer params={{username: userid}}/>
